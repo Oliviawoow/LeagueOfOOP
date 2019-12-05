61c008e5514e48c5820ca1bec5ead5650b209ad9
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-05 23:00:36 +0200

    .

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,12 @@
-public class Player {
 
+/*Clasa Player contine metodele necesare pentru functionarea tuturor eroilor*/
+public abstract class Player {
+    protected String heroType;
+    protected int NPosition;
+    protected int MPosition;
+    protected int startHP;
+    protected int HP;
+    protected int xp;
+    protected int lv;
+    protected boolean Dead;
 }

7a0e86370e80d811c6c20d270f54df5c37972927
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-06 01:03:37 +0200

    .

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,5 @@
 
-/*Clasa Player contine metodele necesare pentru functionarea tuturor eroilor*/
+/*clasa Player contine metodele necesare pentru functionarea tuturor eroilor*/
 public abstract class Player {
     protected String heroType;
     protected int NPosition;
@@ -9,4 +9,34 @@ public abstract class Player {
     protected int xp;
     protected int lv;
     protected boolean Dead;
+
+    public Player(final String heroType) {
+        this.heroType = heroType;
+        this.xp = 0;
+        this.lv = 0;
+        this.Dead = false;
+    }
+
+    /*getteri pentru eroii derivati din clasa Player*/
+    public abstract int getNPosition();
+    public abstract int getMPosition();
+    public abstract int getStartHP();
+    public abstract int getHP();
+    public abstract int getXp();
+    public abstract int getLv();
+    public abstract int getLvUp();
+    public abstract boolean isDead();
+
+    /*setteri pentru eroii derivati din clasa Player*/
+    /*creste Xp dupa o lupta castigata*/
+    public abstract void setXpUP(Player Enemy);
+    /*verifica daca poate face level up si daca da modifica damage-ul si
+     hp-ul initial si hp-ul curent redevine maxim*/
+    public abstract void lvUp();
+    /*verifica daca e mort si daca e adevarat il scoate de pe harta*/
+    public abstract void setDead();
+    /*scade viata curenta in functie de damage-ul primit*/
+    public abstract void takeDmg(int dmg);
+
+
 }

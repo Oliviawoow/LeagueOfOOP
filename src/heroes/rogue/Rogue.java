cd64618f74a0b335e41b1356e754f6aa257e333b
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-06 23:36:57 +0200

    .

--- src/heroes/rogue/Rogue.java
+++ src/heroes/rogue/Rogue.java
@@ -1,123 +1,83 @@
 package heroes.rogue;
 
 import heroes.Player;
+import heroes.PlayerAbilities;
+import heroes.knight.Knight;
+import heroes.knight.KnightAbilities;
+import heroes.pyromancer.Pyromancer;
 import heroes.pyromancer.PyromancerAbilities;
+import heroes.wizard.Wizard;
+import heroes.wizard.WizardAbilities;
+import main.Map;
 
 public class Rogue extends Player {
-    protected String heroType;
-    protected int NPosition;
-    protected int MPosition;
     protected int startHp = 600;
-    protected int hp;
-    protected int xp;
-    protected int lv;
-    protected boolean Dead;
 
-    public Rogue(final int N, final int M) {
-        super("R");
-        this.heroType = "R";
-        this.NPosition = N;
-        this.MPosition = M;
-        this.Dead = false;
+    public Rogue(final int N, final int M, final Map map) {
+        super("R", map, new RogueAbilities(), N, M);
     }
 
-    public final int getStartHp() {
-        return this.startHp;
-    }
-    public final int getHp() {
-        return this.hp;
-    }
-    public final int getXp() {
-        return this.xp;
-    }
-    public final int getLv() {
-        return this.lv;
-    }
-    public final boolean getDead() {
-        return this.Dead;
-    }
-    public final int getNPosition() {
-        return this.NPosition;
-    }
-    public final int getMPosition() {
-        return this.MPosition;
+    public void isAttackedBy(Pyromancer attacker) {
+        int damageFireblast = Math.round(this.getAbilities().ability1(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int damageIgnite = Math.round(this.getAbilities().ability2(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int totalDmg = damageFireblast + damageIgnite;
+        this.takeDmg(totalDmg);
     }
 
-    public final void direction(final char dir) {
-        boolean ok = true;
-        if (dir == 'U' && ok) {
-            this.NPosition = this.NPosition --;
-            ok = false;
-        }
-        if (dir == 'D' && ok) {
-            this.NPosition = this.NPosition ++;
-            ok = false;
-        }
-        if (dir == 'L' && ok) {
-            this.MPosition = this.MPosition --;
-            ok = false;
-        }
-        if (dir == 'R' && ok) {
-            this.MPosition = this.MPosition ++;
-            ok = false;
-        }
-        if (dir == '_' && ok) {
-            ok = false;
+    public void isAttackedBy(Knight attacker) {
+        int damageExecute =Math.round(this.getAbilities().ability1(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int damageSlam =Math.round(this.getAbilities().ability2(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int totalDmg = damageExecute + damageSlam;
+        if (this.getHp() <= this.startHp * Math.min(0.4f, 0.2f + 0.1f * getLvUp())){
+            this.setDead();
+        } else {
+            this.takeDmg(totalDmg);
         }
     }
 
-    public final float mod(final Map map) {
-        float mod;
-        char x = map.getType(this.NPosition, this.MPosition);
-        if (x == 'W') {
-            mod = 0.15f;
-            return mod;
-        }
-        return 0f;
+    public void isAttackedBy(Rogue attacker) {
+        int damageBackstab =Math.round(this.getAbilities().ability1(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int damageParalysis =Math.round(this.getAbilities().ability2(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int totalDmg = damageBackstab + damageParalysis;
     }
 
-    public final int getLvUp() {
-        int nrLvUp;
-        nrLvUp = ((this.xp - 250) / 50 + 1) - this.lv;
-        return nrLvUp;
+    public void isAttackedBy(Wizard attacker) {
+        int damageDrain = Math.round(this.getAbilities().ability1(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int damageDeflect =Math.round(this.getAbilities().ability2(this)
+                * attacker.getAbilities().terrainModifier(attacker.getMap(),
+                attacker.getNPosition(), attacker.getMPosition()));
+        int totalDmg = damageDeflect + damageDrain;
     }
 
-    public final void setXpUp(final Player Enemy) {
-        this.xp = this.xp + Math.max(0, 200 - (this.lv - Enemy.getLv()) * 40);
+    public void attackPlayer(Player enemy) {
+        enemy.isAttackedBy(this);
     }
 
     public final void lvUp() {
         if (this.getLvUp() > 0) {
-            this.startHp = this.startHp + 50 * this.getLvUp();
-            this.hp = this.startHp;
-            this.lv = this.lv + this.getLvUp();
-            PyromancerAbilities.instance.dmgUp(this.getLvUp());
+            this.startHp = this.startHp + 40 * this.getLvUp();
+            this.setHp(this.startHp);
+            this.setLv(this.getLv() + this.getLvUp());
+            this.getAbilities().dmgUp(this.getLvUp());
         }
-    }
-
-    public final void setDead() {
-        if (this.getHp() <= 0) {
-            this.hp = 0;
-            this.NPosition = -1;
-            this.MPosition = -1;
-        }
-    }
-
-    public final void takeDmg(int dmg) {
-        this.hp = this.hp - dmg;
     }
 
     public final boolean roundEffect() {
-
+        return false;
     }
 
-    public final void dmgTake(final Player Enemy, final Map map) {
-        Enemy.takeDmg(Math.round(PyromancerAbilities.instance.getTotalDmg(Enemy, map)
-                * this.mod(map)));
-    }
-
-    public final int dmgNotModTake(final Player Enemy, final Map map) {
-        return Math.round(PyromancerAbilities.instance.getDmgNotMod(Enemy, map)
-                * this.mod(map));
-    }
 }

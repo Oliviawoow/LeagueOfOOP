3f467cb7a1139fbf98c9a87381075459082cee33
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-06 11:30:24 +0200

    .

---
+++ src/PyromancerAbilities.java
@@ -0,0 +1,10 @@
+public class PyromancerAbilities extends PlayerAbilities {
+    protected int dmg1 = 350;
+    protected int dmg2 = 150;
+    protected int dmgPerRound = 50;
+    public static final PyromancerAbilities instance = new PyromancerAbilities();
+
+    public PyromancerAbilities() {
+        super();
+    }
+}

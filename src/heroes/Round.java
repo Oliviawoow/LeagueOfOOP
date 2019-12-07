80b15769dc12ad0691fe3761fbef0a3b496c2357
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-07 17:38:24 +0200

    .

--- src/heroes/Round.java
+++ src/heroes/Round.java
@@ -1,13 +1,12 @@
 package heroes;
 
 import main.Map;
-import heroes.Player;
 
 public class Round {
     public Round() {
 
     }
-
+    /*batalia propriu-zisa*/
      public final void battle(final Player player1, final Player player2) {
         if (!player1.isDead() && !player2.isDead()) {
             player1.attackPlayer(player2);
@@ -26,7 +25,7 @@ public class Round {
 
     public final void gameRounds(final Player[] player, final int nrPlayer, final Map map, final String direction) {
         /*misca eroul*/
-        for (int i = 0; i < nrPlayer; i ++) {
+        for (int i = 0; i < nrPlayer; i++) {
             if (!player[i].getRoundStun() && !player[i].isDead()) {
                 player[i].direction(direction.charAt(i));
             }
@@ -34,7 +33,8 @@ public class Round {
                 player[i].setDead();
             }
         }
-        for (int i = 0; i < nrPlayer; i ++) {
+        /*incepe batalia*/
+        for (int i = 0; i < nrPlayer; i++) {
             for (int j = i + 1; j < nrPlayer; j ++) {
                 if (player[i].getNPosition() == player[j].getNPosition()
                 && player[i].getMPosition() == player[j].getMPosition()) {

4fe9b1c7121da9c6e69a10b16675328cca1036ca
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-07 16:26:02 +0200

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import fileio.implementations.FileReader;
 import fileio.implementations.FileWriter;
+import heroes.Player;
 
 
 public class Main {
@@ -23,6 +24,56 @@ public class Main {
             FileReader read = new FileReader(fileIn);
             int n = read.nextInt();
             int m = read.nextInt();
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                s.append(read.nextWord());
+            }
+            Map map = new Map(n, m, s.toString());
+            int nrPlayers = read.nextInt();
+            Player[] player = new Player[nrPlayers];
+            for (int i = 0; i < nrPlayers; i++) {
+                player[i] = PlayerGenerator.instance.generatePlayer(read.nextWord(),
+                        read.nextInt(), read.nextInt());
+            }
+
+            /*creere de runde*/
+            int nrRounds = read.nextInt();
+            while (nrRounds != 0) {
+                String directions = read.nextWord();
+                round.gameRounds(player, nrPlayers, map, directions);
+                nrRounds--;
+            }
+            /*scrierea in fisier*/
+            try {
+                FileWriter write = new FileWriter(fileOut);
+                for (int i = 0; i < nrPlayers - 1; i++) {
+                    if (player[i].isDead()) {
+                        write.writeWord(player[i].getHeroType() + " " + "dead");
+                        write.writeNewLine();
+                    } else {
+                        write.writeWord(player[i].getHeroType() + " " + player[i].getLv()
+                                + " " + player[i].getXp() + " "
+                                + player[i].getHp() + " " + player[i].getNPosition()
+                                + " " + player[i].getNPosition());
+                        write.writeNewLine();
+                    }
+                }
+                int i = nrPlayers - 1;
+                if (player[i].isDead()) {
+                    write.writeWord(player[i].getHeroType() + " " + "dead");
+                } else {
+                    write.writeWord(player[i].getHeroType() + " " + player[i].getLv()
+                            + " " + player[i].getXp() + " "
+                            + player[i].getHp() + " " + player[i].getNPosition()
+                            + " " + player[i].getMPosition());
+                }
+                write.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
+

e5cd5bbdc852aa92314c8ee6eb07cd1c2d9bcd72
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-07 15:52:15 +0200

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+import heroes.Round;
+
 public class Main {
-    private Main() { }
+    private Main() {
+
+    }
 
     public static void main(final String[] args) {
+        final Round round = new Round();
 
     }
 }

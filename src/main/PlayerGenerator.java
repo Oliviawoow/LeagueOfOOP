4075e4679b44ce4d7cce900e59d2fed55c880289
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-07 18:47:40 +0200

    .

--- src/main/PlayerGenerator.java
+++ src/main/PlayerGenerator.java
@@ -5,6 +5,7 @@ import heroes.knight.Knight;
 import heroes.pyromancer.Pyromancer;
 import heroes.rogue.Rogue;
 import heroes.wizard.Wizard;
+import main.Map;
 
 /*creaza un erou de un anumit tip*/
 public final class PlayerGenerator {
@@ -14,15 +15,15 @@ public final class PlayerGenerator {
     }
 
     /*in functie de tip se defineste eroul si pozitia lui initiala pe harta*/
-    public Player generatePlayer(final String type, final int N, final int M) {
+    public Player generatePlayer(final String type, final int N, final int M, final Map map) {
         if (type.equals("K")) {
-            return new Knight(N, M);
+            return new Knight(N, M, map);
         } else if (type.equals("P")) {
-            return new Pyromancer(N, M);
+            return new Pyromancer(N, M, map);
         } else if (type.equals("R")) {
-            return new Rogue(N, M);
+            return new Rogue(N, M, map);
         } else if (type.equals("W")) {
-            return new Wizard(N, M);
+            return new Wizard(N, M, map);
         } else {
             return null;
         }

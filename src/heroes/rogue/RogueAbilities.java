ade5549569ca1a5543673b29cfee99638ea68a39
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-07 02:07:48 +0200

    .

--- src/heroes/rogue/RogueAbilities.java
+++ src/heroes/rogue/RogueAbilities.java
@@ -17,29 +17,29 @@ public class RogueAbilities extends PlayerAbilities {
     }
 
     public final float getFirstAbilityClassModifier (final Pyromancer enemy) {
-        return 0.25f;
+        return 1.25f;
     }
     public final float getFirstAbilityClassModifier (final Knight enemy) {
-        return -0.1f;
+        return 0.9f;
     }
     public final float getFirstAbilityClassModifier (final Wizard enemy) {
-        return 0.25f;
+        return 1.25f;
     }
     public final float getFirstAbilityClassModifier (final Rogue enemy) {
-        return 0.2f;
+        return 1.2f;
     }
 
     public final float getSecondAbilityClassModifier (final Pyromancer enemy) {
-        return 0.2f;
+        return 1.2f;
     }
     public final float getSecondAbilityClassModifier (final Knight enemy) {
-        return -0.2f;
+        return 0.8f;
     }
     public final float getSecondAbilityClassModifier (final Wizard enemy) {
-        return 0.25f;
+        return 1.25f;
     }
     public final float getSecondAbilityClassModifier (final Rogue enemy) {
-        return -0.1f;
+        return 0.9f;
     }
 
     public final void dmgUp(int nrLv) {
@@ -51,7 +51,7 @@ public class RogueAbilities extends PlayerAbilities {
     public final float terrainModifier(final Map map, final int NPosition, final int MPosition) {
         char type = map.getType(NPosition, MPosition);
         if (type == 'W') {
-            return 0.15f;
+            return 1.15f;
         }
         return 0f;
     }

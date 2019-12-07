d5e1f4b790421b4a1af9fe3109b99584c3f13bf3
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-07 16:05:34 +0200

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,27 @@ package main;
 
 import heroes.Round;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import fileio.implementations.FileReader;
+import fileio.implementations.FileWriter;
+
+
 public class Main {
     private Main() {
 
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
         final Round round = new Round();
+        String fileIn = args[0];
+        String fileOut = args[1];
 
+        /*citire din fisier*/
+        try {
+            FileReader read = new FileReader(fileIn);
+            int n = read.nextInt();
+            int m = read.nextInt();
+        }
     }
 }

08c215e1797c9af9c2426d9322d4b00f5af38839
Olivia Oprea <opreaolivia73@gmail.com>
2019-12-08 16:36:54 +0200

    something

--- src/heroes/wizard/Wizard.java
+++ src/heroes/wizard/Wizard.java
@@ -81,9 +81,9 @@ public class Wizard extends Player {
                 * attacker.getAbilities().terrainModifier(attacker.getMap(),
                 attacker.getNPosition(), attacker.getMPosition());
         damageDrain = damageDrain * Math.min(0.3f * this.startHp, this.getHp());
-        float damageDeflect =Math.round(attacker.getAbilities().ability2(this)
+        float damageDeflect = attacker.getAbilities().ability2(this)
                 * attacker.getAbilities().terrainModifier(attacker.getMap(),
-                attacker.getNPosition(), attacker.getMPosition()));
+                attacker.getNPosition(), attacker.getMPosition());
         damageDeflect = damageDeflect * this.dmgNoModifier();
         int totalDmg = Math.round(damageDeflect) + Math.round(damageDrain);
         this.takeDmg(totalDmg);
